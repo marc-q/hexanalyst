fcd1c15e48aeb27ec3d8783d5a12bb167db30aa1
Marc Volker Dickmann <hq@devdiary.org>
2015-10-11 15:24:56 +0200

    Several improvements.

--- hexanalyst.c
+++ hexanalyst.c
@@ -1,7 +1,10 @@
 /* Copyright 2015 Marc Volker Dickmann */
 /* Project: Hexanalyst */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
+#include "lib/dutils.h"
 
 #define P_CMD "hexanalyst"
 #define P_VERSION "0.0.1"
@@ -9,31 +12,41 @@
 #define PRINTIF(a, b) ((a) >= 21 && (a) <= (126) ? (a) : (b))
 
 /*
-	Function: ha_read_file_hex (char* filename, int seekpos, int start, int end);
-	Description: Reads a file and display its bytes and the bits of the bytes.
+	Function: utils_streq (char* one, char* two);
+	Description: Check's if two strings are equal and have the same length.
 	InitVersion: 0.0.1
 */
-static int ha_read_file_hex (char* filename, int seekpos, int start, int end)
+int utils_streq (char* one, char* two)
 {
-	int i, j, bit;
-	char byte;
-	FILE *afile;
-
-	i = j = bit = 0;
-
-	afile = fopen (filename, "rb");
-
-	if (afile == NULL)
+	if (strlen (one) != strlen (two))
 	{
 		return -1;
 	}
+	
+	return strncmp (one, two, strlen (one));
+}
 
-	fseek (afile, seekpos, SEEK_SET);
-
-	for (i = start; i < end; i++)
+/*
+	Function: ha_view_bits (FILE* bfile, int seekpos, int end);
+	Description: Prints the bits and bytes.
+	InitVersion: 0.0.1
+*/
+static void ha_view_bits (FILE* bfile, int seekpos, int end)
+{
+	int i, j, bit;
+	char byte;
+	
+	i = j = bit = 0;
+	
+	byte = '\0';
+	
+	fseek (bfile, seekpos, SEEK_SET);
+	
+	for (i = 0; i < end && !feof (bfile); i++)
 	{
-		fread (&byte, sizeof (char), 1, afile);
+		fread (&byte, sizeof (char), 1, bfile);
 
+		printf ("| Offset: %i - %x\n", seekpos+i, seekpos+i);
 		printf ("| %c - %i - %x\t", PRINTIF (byte, ' '), byte, byte);
 
 		for (j = 0; j < 8; j++)
@@ -53,32 +66,174 @@ static int ha_read_file_hex (char* filename, int seekpos, int start, int end)
 		}
 		printf ("\n");
 	}
+}
 
-	fclose (afile);
+/*
+	Function: ha_view_ascii (FILE* bfile, int seekpos, int end);
+	Description: Prints the readable ascii.
+	InitVersion: 0.0.1
+*/
+static void ha_view_ascii (FILE* bfile, int seekpos, int end)
+{
+	int i, j;
+	char byte;
+	
+	i = j = 0;
+	
+	byte = '\0';
+	
+	fseek (bfile, seekpos, SEEK_SET);
+	
+	for (i = 0; i < end && !feof (bfile); i++, j++)
+	{
+		fread (&byte, sizeof (char), 1, bfile);
+
+		printf ("%c", PRINTIF (byte, ' '));
+		
+		if (j >= 80)
+		{
+			printf ("\n");
+			j = 0;
+		}
+	}
+	
+	printf ("\n");
+}
+
+/*
+	Function: ha_read_file_binary (char* filename, int view, int seekpos, int end);
+	Description: Reads a file and display its bytes and the bits of the bytes.
+	InitVersion: 0.0.1
+*/
+static int ha_read_file_binary (char* filename, int view, int seekpos, int end)
+{
+	FILE *bfile;
+
+	bfile = fopen (filename, "rb");
+
+	if (bfile == NULL)
+	{
+		return -1;
+	}
+
+	switch (view)
+	{
+		case 0:
+			ha_view_bits (bfile, seekpos, end);
+			break;
+		case 1:
+			ha_view_ascii (bfile, seekpos, end);
+			break;
+		default:
+			break;
+	}
+	
+	fclose (bfile);
 	
 	return 0;
 }
 
+/*
+	Function: print_about (void);
+	Description: Prints the about message.
+	InitVersion: 0.0.1
+*/
+static void print_about (void)
+{
+	printf ("LICENCE: GPL v.3\n");
+}
+
+/*
+	Function: print_help (void);
+	Description: Prints the help message.
+	InitVersion: 0.0.1
+*/
+static void print_help (void)
+{
+	printf ("%s\n", P_CMD);
+	printf ("\tabout\t\t\t\tAbout this programm.\n");
+	printf ("\thelp\t\t\t\tThis help message.\n");
+	printf ("\tdo [FILENAME] [bytes] ARGS\tShow [bytes] from [FILENAME] with optional ARGS.\n");
+	
+	printf ("\nARGS:\n");
+	printf ("-a\t\tUses the ascii view instad of the bit view.\n");
+	printf ("-h\t\tThis help.\n");
+	printf ("-s [seekset]\tSeek [seekset] bytes from the beginning.\n");
+	
+	printf ("\nLEGEND: <optional> [necessary] [integer] [STRING]\n");
+}
+
+
+/*
+	Function: usage (void);
+	Description: Prints the usage message.
+	InitVersion: 0.0.1
+*/
 static void usage (void)
 {
-	printf ("Use: $ %s <filename> <seekfromset> <counterstart> <counterend>\n", P_CMD);
+	printf ("Use: $ %s -h\n", P_CMD);
 }
 
 int main (int argc, char* argv[])
 {
+	int optc, useascii, seekpos;
+	
+	useascii = seekpos = 0;
+	
 	printf ("Hexanalyst v. %s (c) 2015 Marc Volker Dickmann\n\n", P_VERSION);
 
-	if (argc == 5)
+	if (argc == 2)
 	{
-		if (isdigit (argv[2][0]) == 0 ||
-		    isdigit (argv[3][0]) == 0 ||
-		    isdigit (argv[4][0]) == 0)
+		if (utils_streq (argv[1], "help") == 0)
 		{
-			usage ();
+			print_help ();
+			exit (EXIT_SUCCESS);
+		}
+		else if (utils_streq (argv[1], "about") == 0)
+		{
+			print_about ();
+			exit (EXIT_SUCCESS);
+		}
+	}
+
+	while ((optc = getopt (argc, argv, "has:")) != -1)
+	{
+		switch (optc)
+		{
+			case 'h':
+				print_help ();
+				exit (EXIT_SUCCESS);
+				break;
+			case 'a':
+				useascii = 1;
+				break;
+			case 's':
+				if (isdigit (optarg[0]) != 0)
+				{
+					seekpos = atoi (optarg);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	if (argc >= 4)
+	{
+		if (utils_streq (argv[optind], "do") == 0)
+		{
+			if (isdigit (argv[optind+2][0]) == 0)
+			{
+				usage ();
+			}
+			else
+			{
+				ha_read_file_binary (argv[optind+1], useascii, seekpos, atoi (argv[optind+2]));
+			}
 		}
 		else
 		{
-			ha_read_file_hex (argv[1], atoi (argv[2]), atoi (argv[3]), atoi (argv[4]));
+			usage ();
 		}
 	}
 	else
@@ -88,4 +243,3 @@ int main (int argc, char* argv[])
 
 	return 0;
 }
-
---
+++ lib/dutils.h
@@ -0,0 +1,67 @@
+/*
+ * dutils.h
+ * This file is part of DUtils
+ *
+ * Copyright (C) 2015 - Marc Volker Dickmann
+ *
+ * DUtils is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DUtils. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __DUTILS_H__
+#define __DUTILS_H__
+
+/* DUTILS STUFF */
+
+#define DUTILS_VERSION "0.0.1 A"
+
+/* BASH COLORS */
+
+#define BC_TXT_BLACK "\033[0;30m"	/* Black */
+#define BC_TXT_RED "\033[0;31m"		/* Red */
+#define BC_TXT_GREEN "\033[0;32m"	/* Green */
+#define BC_TXT_YELLOW "\033[0;33m"	/* Yellow */
+#define BC_TXT_BLUE "\033[0;34m"	/* Blue */
+#define BC_TXT_PURPLE "\033[0;35m"	/* Purple */
+#define BC_TXT_CYAN "\033[0;36m"	/* Cyan */
+#define BC_TXT_WHITE "\033[0;37m"	/* White */
+
+#define BC_BLD_BLACK "\033[1;30m"	/* Black - Bold */
+#define BC_BLD_RED "\033[1;31m"		/* Red */
+#define BC_BLD_GREEN "\033[1;32m"	/* Green */
+#define BC_BLD_YELLOW "\033[1;33m"	/* Yellow */
+#define BC_BLD_BLUE "\033[1;34m"	/* Blue */
+#define BC_BLD_PURPLE "\033[1;35m"	/* Purple */
+#define BC_BLD_CYAN "\033[1;36m"	/* Cyan */
+#define BC_BLD_WHITE "\033[1;37m"	/* White */
+
+#define BC_UND_BLACK "\033[4;30m"	/* Black - Underline*/
+#define BC_UND_RED "\033[4;31m"		/* Red */
+#define BC_UND_GREEN "\033[4;32m"	/* Green */
+#define BC_UND_YELLOW "\033[4;33m"	/* Yellow */
+#define BC_UND_BLUE "\033[4;34m"	/* Blue */
+#define BC_UND_PURPLE "\033[4;35m"	/* Purple */
+#define BC_UND_CYAN "\033[4;36m"	/* Cyan */
+#define BC_UND_WHITE "\033[4;37m"	/* White */
+
+#define BC_BAK_BLACK "\033[40m"         /* Black - Background */
+#define BC_BAK_RED "\033[41m"           /* Red */
+#define BC_BAK_GREEN "\033[42m"         /* Green */
+#define BC_BAK_YELLOW "\033[43m"        /* Yellow */
+#define BC_BAK_BLUE "\033[44m"          /* Blue */
+#define BC_BAK_PURPLE "\033[45m"        /* Purple */
+#define BC_BAK_CYAN "\033[46m"          /* Cyan */
+#define BC_BAK_WHITE "\033[47m"         /* White */
+
+#define BC_TXT_RST "\033[0m"		/* Text Reset */
+
+#endif /* __DUTILS_H__ */
